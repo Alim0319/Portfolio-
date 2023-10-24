f651c0eb9a228178bdd29b44cb530ddfde709fc2
AlimErk <alim.kodehode@gmail.com>
2023-10-24 14:14:45 +0200

    ;

--- Portfolio/src/Component/HomePage/Homepage.jsx
+++ Portfolio/src/Component/HomePage/Homepage.jsx
@@ -2,6 +2,7 @@ import "./homepage.css";
 import menImg from "../images/Alim.png";
 import codeImg from "../images/coding.png";
 import { useTranslation } from "react-i18next";
+import Typewriter from "typewriter-effect";
 
 function HomePage() {
   const { t } = useTranslation();
@@ -10,10 +11,27 @@ function HomePage() {
     <header className="home">
       <img className="me" src={menImg} alt="Me" />
       <div>
-        <h1>{t("HomePage.hompageTitel")}</h1>
-        <h4>{t("HomePage.profession1")}</h4>
+        <h1>
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString(t("HomePage.hompageTitel"))
+                .pauseFor(1000)
+                .deleteAll()
+                .typeString(t("HomePage.profession1"))
+                .pauseFor(1000)
+                .deleteAll()
+                .typeString(t("HomePage.profession3"))
+                .pauseFor(1000)
+                .deleteAll()
+                .typeString(t("HomePage.profession4"))
+                .start();
+            }}
+          />
+        </h1>
+        {/*<h4>{t("HomePage.profession1")}</h4>
         <h4>{t("HomePage.profession3")}</h4>
-        <h4>{t("HomePage.profession4")}</h4>
+          <h4>{t("HomePage.profession4")}</h4>*/}
       </div>
       <img className="coding" src={codeImg} alt="Coding" />
     </header>
