b6d6980135654446b7b2e26d58d43d1079307837
AlimErk <alim.kodehode@gmail.com>
2023-10-24 13:34:59 +0200

    s

--- Portfolio/src/Component/navbar/navbar.jsx
+++ Portfolio/src/Component/navbar/navbar.jsx
@@ -1,31 +1,28 @@
-import { FaBars, FaTimes, FaHome } from "react-icons/fa";
-import { useRef } from "react";
-import { Link } from "react-router-dom";
+import { FaBars, FaHome } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import resume from "../images/CV_Alim .pdf";
 import LanguageSwitch from "../LanguageSwitch/LanguageSwitch";
-import { useState } from "react";
 import { useTranslation } from "react-i18next";
-
 import "../styles/main.css";
 
 function Navbar() {
-  const navRef = useRef();
+  const [expandNavbar, setExpandNavbar] = useState(false);
+  const location = useLocation();
   const { t } = useTranslation();
-  const [isOpen, setIsOpen] = useState(false);
-  /*const showNavbar = () => {
-    navRef.current.classList.toggle("responsive_nav");
-  };*/
-  const toggleNavbar = () => {
-    setIsOpen(!isOpen); // Rename setIsNavbarOpen to setIsOpen
-  };
+
+  useEffect(() => {
+    setExpandNavbar(false);
+  }, [location]);
+
   return (
-    <header>
+    <header className="navbar" id={expandNavbar ? "open" : "close"}>
       <div className="logo">
         <Link to="/Home">
           <img src="logo-color.png" alt="Logo" />
         </Link>
       </div>
-      <nav ref={navRef} className={isOpen ? "responsive_nav" : ""}>
+      <nav>
         <ul className="nav-list">
           <li>
             <Link to="/Home">
@@ -35,15 +32,11 @@ function Navbar() {
           <li>
             <Link to="/About">{t("translation.about")}</Link>
           </li>
-          {/*<li>
-            <Link to="/Skills">{text[selectedLanguage].skills}</Link>
-  </li>*/}
           <li>
             <Link to="/Project">{t("translation.Projects")}</Link>
           </li>
           <li>
             <a href={resume} download>
-              {" "}
               {t("translation.Download Resume")}
             </a>
           </li>
@@ -54,15 +47,17 @@ function Navbar() {
             <LanguageSwitch />
           </li>
         </ul>
-
-        <button className="nav-btn nav-close-btn" onClick={toggleNavbar}>
-          <FaTimes />
-        </button>
       </nav>
-
-      <button className="nav-btn" onClick={toggleNavbar}>
-        <FaBars />
-      </button>
+      <div className="toggleButton">
+        <button
+          onClick={() => {
+            setExpandNavbar((prev) => !prev);
+          }}
+          name="Menu"
+        >
+          <FaBars />
+        </button>
+      </div>
     </header>
   );
 }
